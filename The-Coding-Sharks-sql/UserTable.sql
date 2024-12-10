359a7aed0be05d2bd74323962ba8529f5f256c6e
nicolepeak <colecpeak@gmail.com>
2024-12-10 17:37:01 -0600

    updates to randomDestination

--- The-Coding-Sharks-sql/UserTable.sql
+++
@@ -1,9 +0,0 @@
-create table UserTable (
-    UserID int,
-    Username varchar(255),
-    Password varchar(255),
-    Email varchar(255),
-    FirstName varchar(255),
-    LastName varchar(255),
-    
-);
